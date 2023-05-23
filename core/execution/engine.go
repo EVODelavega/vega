9a364b88fd3368d684f0cae6cb76b2fc0eb301dd
Elias Van Ootegem <elias@vega.xyz>
2023-05-23 13:30:44 +0100

    feat: fix silly error
    Signed-off-by: Elias Van Ootegem <elias@vega.xyz>

--- core/execution/engine.go
+++ core/execution/engine.go
@@ -379,7 +379,7 @@ func (e *Engine) succeedOrRestore(ctx context.Context, successor, parent string,
 			parentState = pmo.GetCPState()
 		}
 	}
-	if !insuranceFraction {
+	if !insuranceFraction.IsZero() {
 		assets, _ := mkt.mkt.GetAssets()
 		lm := e.collateral.SuccessorInsuranceFraction(ctx, successor, parent, assets[0], insuranceFraction)
 		e.broker.Send(events.NewLedgerMovements(ctx, []*types.LedgerMovement{lm}))
